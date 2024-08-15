85c94d86f22aa89b5860e489783b4233f935ab38
Olarewaju Mojeed Olalekan <53194190+Lowkey1729@users.noreply.github.com>
2024-08-16 00:53:43 +0100

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from blog.models import Post
+
+
+# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status', 'created']
+    prepopulated_fields = {'slug': ('title',)}
---
+++ blog/models.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
+
+# Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft',
+        PUBLISHED = 'PB', 'Published',
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    body = models.TextField()
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+
+    )
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
