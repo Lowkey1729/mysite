42c063c422f32dd6d33e2fa9bd9b866f6986ac2b
Olarewaju Mojeed Olalekan <53194190+Lowkey1729@users.noreply.github.com>
2024-08-17 20:42:23 +0100

    More progress

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+import json
+
+from django.core import serializers
+from django.core.paginator import Paginator
+from django.core.serializers import serialize
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
@@ -5,12 +11,22 @@ from blog.models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    _post_list = Post.published.all()
+    paginator = Paginator(_post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+    serialized_data = json.loads(serialize('json', posts))
+    return JsonResponse(serialized_data, safe=False)
 
 
-def post_detail(request, id):
-    post = get_object_or_404(
+def post_detail(request, year, month, day, post):
+    _post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
     )
+
+    return HttpResponse(serializers.serialize('json', _post))
