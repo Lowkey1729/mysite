d22af177e78dbb84b002879536c4649450a38b2f
Olarewaju Mojeed Olalekan <53194190+Lowkey1729@users.noreply.github.com>
2024-08-17 22:49:21 +0100

    More progress

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,9 @@ from django.core.serializers import serialize
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
+from blog.forms import EmailPostForm
 from blog.models import Post
 
 
@@ -31,6 +33,42 @@ def post_detail(request, year, month, day, post):
     )
 
     return HttpResponse(serializers.serialize('json', _post))
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if not form.is_valid():
+            form = EmailPostForm()
+        else:
+            cleaned_data = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cleaned_data['name'] ({cleaned_data['email']})}"
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cleaned_data['name']}\'s comments: {cleaned_data['comments']}"
+            )
+
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cleaned_data['to']]
+            )
+
+            sent = True
+
 
 
 class PostListView(ListView):
